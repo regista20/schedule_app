3c81e72628487d1a097c6690603f4664bf8d9a50
yuki <regista20ys@gmail.com>
2013-08-02 20:29:35 +0900

    Finish static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,63 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+
+  let(:base_title) { "Schedule App" }
+
+  describe "Home page" do
+
+    it "should have the h1 'Schedule App'" do
+      visit '/static_pages/home'
+      page.should have_selector('h1', :text => 'Schedule App')
+    end
+
+    it "should have the title 'Home'" do
+      visit '/static_pages/home'
+      page.should have_selector('title', :text => "#{base_title}")
+    end
+
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      page.should_not have_selector('title', :text => '| Home')
+    end
+  end
+
+  describe "Help page" do
+
+    it "should have the h1 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('h1', :text => 'Help')
+    end
+
+    it "should have the title 'Help'" do
+      visit '/static_pages/help'
+      page.should have_selector('title', :text => "#{base_title} | Help")
+    end
+  end
+
+  describe "About page" do
+
+    it "should have the h1 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('h1', :text => 'About Us')
+    end
+
+    it "should have the title 'About'" do
+      visit '/static_pages/about'
+      page.should have_selector('title', :text => "#{base_title} | About")
+    end
+  end
+
+  describe "Contact page" do
+
+    it "should have the h1 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('h1', :text => 'Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      page.should have_selector('title', :text => "#{base_title} | Contact")
+    end
+  end
+end
